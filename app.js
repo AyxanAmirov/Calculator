88513c68ce584fe01a2e5ec00e4e210e43e5935b
AyxanAmirov <amirovayxan0@gmail.com>
2024-08-28 15:47:04 -0700

    calac update

--- app.js
+++ app.js
@@ -2,15 +2,32 @@ const defaultValue = document.querySelector(".value");
 const values = document.querySelectorAll(".calc-item,.calc-item-2, .calc-zero");
 const deleteAllBtn = document.querySelector("#deleteAll");
 const deleteBtn = document.querySelector("#delete");
+const maths = document.querySelectorAll(".math");
+
+function runEvents() {
+  deleteAllBtn.addEventListener("click", deleteAll);
+  deleteBtn.addEventListener("click", deleteValue);
+}
+runEvents();
 
 values.forEach((value) => {
   value.addEventListener("click", () => {
     const operators = ["/", "*", "+", "-", "%", "."];
+    const operatorsArr = ["/", "*", "+", "-", "%"];
     const lastChar = defaultValue.textContent.slice(-1);
-
+    let operatorIndex = -1;
+    let operator = "";
+    for (let i = 0; i < operators.length; i++) {
+      const index = defaultValue.textContent.indexOf(operators[i]);
+      if (index !== -1) {
+        operatorIndex = index;
+        operator = operators[i];
+        break;
+      }
+    }
     if (
       defaultValue.textContent === "0" &&
-      operators.includes(value.textContent.trim())
+      operatorsArr.includes(value.textContent.trim())
     ) {
       return;
     }
@@ -27,17 +44,31 @@ values.forEach((value) => {
     } else {
       defaultValue.textContent += value.textContent.trim();
     }
+    if (operatorIndex !== -1) {
+      const leftSide = defaultValue.textContent.slice(0, operatorIndex);
+      const rightSide = defaultValue.textContent.slice(operatorIndex + 1);
 
+      switch (operator) {
+        case "+":
+          defaultValue.textContent = +leftSide + +rightSide;
+          break;
+        case "-":
+          defaultValue.textContent = +leftSide - +rightSide;
+          break;
+        case "*":
+          defaultValue.textContent = +leftSide * +rightSide;
+          break;
+        case "/":
+          defaultValue.textContent = +leftSide / +rightSide;
+          break;
+        case "%":
+          defaultValue.textContent = +leftSide % +rightSide;
+          break;
+      }
+    }
     defaultValue.scrollLeft = defaultValue.scrollWidth;
   });
 });
-
-runEvents();
-
-function runEvents() {
-  deleteAllBtn.addEventListener("click", deleteAll);
-  deleteBtn.addEventListener("click", deleteValue);
-}
 
 function deleteAll() {
   defaultValue.textContent = "0";
